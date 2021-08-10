6c9eac8e437468cc579347c897c390705477060b
GopalSreedharanHM <gsreedhara@wiley.com>
2021-08-10 22:10:09 +0530

    Updated Dim SQL Scripts

--- models/Data_To_Insights/CustomerCampaignMapping.sql
+++ models/Data_To_Insights/CustomerCampaignMapping.sql
@@ -1,3 +1,9 @@
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+    Try changing "table" to "view" below
+*/
 {{
   config(
     materialized = 'incremental',
@@ -7,11 +13,18 @@
 }}
 
 SELECT
-ROW_NUMBER() OVER (ORDER BY id) AS mappingid, 
+ROW_NUMBER() OVER (ORDER BY campaign_id,customer_id) AS mappingid, 
 campaign_id as campaignid,
 customer_id as customerid,
 to_date(campaign_date,'mm/dd/yyyy') as campaigndateid,
 is_success as issuccess,
-to_timestamp(_FIVETRAN_SYNCED) as createddate,
+_FIVETRAN_SYNCED as createddate,
 'fivetran' as createdby
 from data_to_insights_raw.stg_customertocampaing
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  WHERE _FIVETRAN_SYNCED > (select max(createddate) from  {{ this }})
+
+{% endif %}
