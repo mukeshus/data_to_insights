7f00752077b0119db7d15f30871556ec60699d33
GopalSreedharanHM <gsreedhara@wiley.com>
2021-08-16 15:22:46 +0530

    Update FactSales script

---
+++ models/Data_To_Insights/FactSales.sql
@@ -0,0 +1,54 @@
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+    Try changing "table" to "view" below
+*/
+
+{{
+    config(
+        materialized='incremental',
+		unique_key='FACTSALESID',
+		merge_update_columns = ['campaignid', 'customerid','ORDERDATEID','ORDERLOCATIONID','PRODUCTID','SHIPDATEID']
+    )
+}}
+
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY sal.value_row_id) AS FACTSALESID
+    , cust.customerid AS CUSTOMERID 
+	, camp.campaignid AS CAMPAIGNID 
+	, ddorder.date AS ORDERDATEID 
+	, dl.locationid AS ORDERLOCATIONID 
+	, dp.productid AS PRODUCTID 
+	, ddship.date AS SHIPDATEID
+	, SAL.VALUE_SHIP_MODE AS SHIPMODE
+	, SAL.VALUE_DISCOUNT AS DISCOUNT
+	, SAL.VALUE_PROFIT AS PROFIT
+	, SAL.VALUE_NUMBER_OF_RECORDS AS QUANTITY
+	, SAL.VALUE_NUMBER_OF_RECORDS * SAL.VALUE_SHIPPING_COST AS SALEAMOUNT
+	, SAL.VALUE_SHIPPING_COST AS SHIPPINGCOST
+	, TO_TIMESTAMP(SAL.VALUE_CREATED_DATE) AS CREATEDDATE 
+	, 'fivetran' AS  CREATEDBY 
+    , _FIVETRAN_SYNCED AS MODIFIEDDATE
+    , 'fivetran' AS  MODIFIEDBY
+FROM 
+    DATA_TO_INSIGHTS.DATA_TO_INSIGHTS_RAW.STAGE_SALES_DATA SAL
+	LEFT OUTER JOIN DATA_TO_INSIGHTS.DATA_TO_INSIGHTS.DIMCUSTOMER cust on cust.CUSTOMERSOURCEKEY=sal.VALUE_CUSTOMER_ID
+	LEFT OUTER JOIN DATA_TO_INSIGHTS.DATA_TO_INSIGHTS.DIMCAMPAIGN camp on camp.CAMPAIGNSOURCEKEY=sal.VALUE_CAMPAIGN_ID
+	LEFT OUTER JOIN DATA_TO_INSIGHTS.DATA_TO_INSIGHTS.DIMDATE ddorder on ddorder.date=to_date(sal.value_order_date,'mm/dd/yyyy')
+	LEFT OUTER JOIN DATA_TO_INSIGHTS.DATA_TO_INSIGHTS.DIMDATE ddship on ddship.date=to_date(sal.value_ship_date,'mm/dd/yyyy')
+	LEFT OUTER JOIN DATA_TO_INSIGHTS.DATA_TO_INSIGHTS.DIMLOCATION dl ON dl.region=sal.VALUE_REGION and dl.state=sal.VALUE_STATE 
+																	and dl.city=sal.VALUE_CITY and dl.country=sal.VALUE_COUNTRY
+	LEFT OUTER JOIN DATA_TO_INSIGHTS.DATA_TO_INSIGHTS.DIMPRODUCT dp on dp.productsourcekey=sal.value_product_id
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  WHERE _FIVETRAN_SYNCED > (select max(MODIFIEDDATE) from  {{ this }})
+
+{% endif %}
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
