32756a653407f6e899d73fe28580b087f859e80a
GopalSreedharanHM <gsreedhara@wiley.com>
2021-08-30 16:04:12 +0530

    customercampaignmapping update

--- models/Data_To_Insights/CustomerCampaignMapping.sql
+++ models/Data_To_Insights/CustomerCampaignMapping.sql
@@ -3,6 +3,7 @@
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
     Try changing "table" to "view" below
+	Updated Customer & Campaign ID's with Dim Keys
 */
 {{
   config(
