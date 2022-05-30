2d98686b8fd3d03ecef4907528253ca3beae9de3
mukesh.us <mukesh.us@happiestminds.com>
2022-05-30 23:55:46 +0530

    Reading DimCustomer metadata from the generic template

--- models/Data_To_Insights/DimCustomer.sql
+++ models/Data_To_Insights/DimCustomer.sql
@@ -1,10 +1,3 @@
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-    Try changing "table" to "view" below
-*/
-
 {{
     config(
         materialized='incremental',
@@ -13,21 +6,122 @@
     )
 }}
 
+--Pull the Customer attributes from the template
+
+--customersourcekey
+
+{% set customersourcekey = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'CUSTOMERSOURCEKEY\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set results_list = customersourcekey.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+--FIRSTNAME
+
+{% set firstname = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'FIRSTNAME\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set firstname_list = firstname.columns[0].values() %}
+{% else %}
+{% set firstname_list = [] %}
+{% endif %}
+
+--LASTNAME
+
+{% set lastname = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'LASTNAME\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set lastname_list = lastname.columns[0].values() %}
+{% else %}
+{% set lastname_list = [] %}
+{% endif %}
+
+--AGE
+
+{% set age = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'AGE\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set age_list = age.columns[0].values() %}
+{% else %}
+{% set age_list = [] %}
+{% endif %}
+
+--GENDER
+
+{% set gender = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'GENDER\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set gender_list = gender.columns[0].values() %}
+{% else %}
+{% set gender_list = [] %}
+{% endif %}
+
+--EMAIL
+
+{% set email = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'EMAIL\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set email_list = email.columns[0].values() %}
+{% else %}
+{% set email_list = [] %}
+{% endif %}
+
+--ADDRESS
+
+{% set address = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'ADDRESS\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set address_list = address.columns[0].values() %}
+{% else %}
+{% set address_list = [] %}
+{% endif %}
+
+--ISACTIVE
+
+{% set isactive = run_query('select CUSTOMER_ATTRIBUTE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'ISACTIVE\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set isactive_list = isactive.columns[0].values() %}
+{% else %}
+{% set isactive_list = [] %}
+{% endif %}
+
+--TABLE NAME
+
+{% set cust_tablename = run_query('select Top 1 CUSTOMER_ENTITY_TABLE_NAME from "DATA_TO_INSIGHTS"."HM_AWS_S3"."MAPPING_TEMPLATE" WHERE D_2_I_ATTRIBUTE_NAME = \'CUSTOMERSOURCEKEY\' AND D_2_I_ENTITY_NAME = \'Customer\'') %}
+{% if execute %}
+{% set cust_tablename_list = cust_tablename.columns[0].values() %}
+{% else %}
+{% set cust_tablename_list = [] %}
+{% endif %}
+
     SELECT 
-	ROW_NUMBER() OVER (ORDER BY s.CUSTOMERID) AS CUSTOMERID
-    , s.CUSTOMERID AS CUSTOMERSOURCEKEY 
-	, s.LASTNAME ||', '||s.FIRSTNAME AS CUSTOMERNAME 
-	, s.AGE AS AGE 
-	, s.GENDER   GENDER 
-	, S.EMAIL  EMAIL 
-	, ' ' AS ADDRESS
-	, 1 AS ISACTIVE
+    {% for customersourcekey in results_list %}
+	ROW_NUMBER() OVER (ORDER BY {{customersourcekey}}) AS CUSTOMERID
+    ,{{customersourcekey}} AS CUSTOMERSOURCEKEY 
+    {% endfor %}
+	{% for firstname in firstname_list %}
+	{% for lastname in lastname_list %}
+		, {{firstname}} ||', '||{{lastname}} AS CUSTOMERNAME 
+	 {% endfor %}
+	 {% endfor %}
+	 {% for age in age_list %}
+		, {{age}} AS AGE 
+	 {% endfor %}
+	 {% for gender in gender_list %}
+		, {{gender}}   GENDER 
+	 {% endfor %}
+	 {% for email in email_list %}
+		, {{email}}  EMAIL 
+	 {% endfor %}
+	 {% for address in address_list %}
+		, {{address}} AS ADDRESS
+     {% endfor %}
+	 {% for isactive in isactive_list %}
+		, {{isactive}} AS ISACTIVE
+	 {% endfor %}
 	, current_timestamp() AS CREATEDDATE 
 	, 'fivetran' AS  CREATEDBY 
     , _FIVETRAN_SYNCED AS MODIFIEDDATE
     , 'fivetran' AS  MODIFIEDBY
 FROM 
-    DATA_TO_INSIGHTS.RDS_RAW_DATA_DBO.CUSTOMERMASTER S
+    {% for cust_tablename in cust_tablename_list %}
+    DATA_TO_INSIGHTS.RDS_RAW_DATA_DBO.{{cust_tablename}} S
+	{% endfor %}
 
 {% if is_incremental() %}
 
@@ -35,8 +129,3 @@ FROM
   WHERE _FIVETRAN_SYNCED > (select max(MODIFIEDDATE) from  {{ this }})
 
 {% endif %}
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
